72e2cfb62e9c30b2cd5a35ecebebe2257e2b95b6
Adam Smyth <Smyth-A81@ulster.ac.uk>
2025-12-08 17:28:38 +0000

    Final changes done

--- Assets/Scripts/RopeCollsion.cs
+++ Assets/Scripts/RopeCollsion.cs
@@ -6,6 +6,17 @@ public class RopeCollsion : MonoBehaviour
     private bool isDead;
 
     public GameManager gameManager;
+    public GameObject deathEffect;
+
+    public AudioSource audioSource;
+    public AudioClip playerFallNoise;
+    public AudioSource audioSource2; 
+    public AudioClip ropeThump; 
+
+    public void Awake()
+    {
+        deathEffect.SetActive(false);
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -13,21 +24,37 @@ public class RopeCollsion : MonoBehaviour
 
         if (other.gameObject.tag == "Rope")
         {
+            audioSource2.PlayOneShot(ropeThump);
+
             Debug.Log("Rope Hit");
-            // Destroy(this.gameObject);
+
+
             StartCoroutine(WaitAndDie());
+        }
+        else if(other.gameObject.tag == "Death")
+        {
+            audioSource.PlayOneShot(playerFallNoise);
+            deathEffect.SetActive(true);
+            Debug.Log("Player DEAD");
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            gameManager.gameOver();
+            Destroy(this.gameObject);
         }
     }
 
     IEnumerator WaitAndDie ()
     {
+        
         yield return new WaitForSeconds(1f);
 
+        audioSource.PlayOneShot(playerFallNoise); 
+        deathEffect.SetActive(true);
         Debug.Log("Player DEAD");
-        Destroy(this.gameObject);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         gameManager.gameOver();
+        Destroy(this.gameObject);
     }
 
 }
